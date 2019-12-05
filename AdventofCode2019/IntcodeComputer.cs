204b08f5c1285ad606e923b463e380b1208c19bf
Moritz <moritz.wahlenmeier@yahoo.de>
2019-12-06 00:50:23 +0100

    day 5 commit

--- AdventofCode2019/IntcodeComputer.cs
+++ AdventofCode2019/IntcodeComputer.cs
@@ -11,13 +11,17 @@ namespace AdventofCode2019
     {
         private int[] intcode;
 
+        public IntcodeComputer(int[] intcode)
+        {
+            this.intcode = intcode;
+        }
         public IntcodeComputer(string url)
         {
             try
             {
                 StreamReader sr = new StreamReader(url);
                 string s = sr.ReadToEnd();
-                string[] data = s.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] data = s.Split(new char[] { ',','\n','\t',' ','\r' }, StringSplitOptions.RemoveEmptyEntries);
 
                 intcode = new int[data.Length];
 
@@ -46,21 +50,125 @@ namespace AdventofCode2019
         {
             int[] intcodeR = intcode;
             bool _continue = true;
-            for(int i = 0; i < intcode.Length&&_continue; i += 4)
+            int opcode, modeP1, modeP2, modeP3,instruction,index1,index2,index3,instructionLength=0;
+            for(int i = 0; i < intcode.Length&&_continue; i += instructionLength)
             {
-                switch (intcodeR[i])
+                instruction = intcodeR[i];
+
+                modeP3 = instruction / 10000;
+                instruction %= 10000;
+                modeP2 = instruction / 1000;
+                instruction %= 1000;
+                modeP1 = instruction / 100;
+                instruction %= 100;
+                opcode = instruction;
+
+                int[] index = new int[] { 0, 0, 0 };
+                if (i+3 < intcode.Length)
+                {
+                    index = GetIndex(i, modeP1, modeP2, modeP3, intcodeR[i + 1], intcodeR[i + 2], intcodeR[i + 3]);
+                }    
+                switch (opcode)
                 {
                     case 1:
-                        intcodeR[intcodeR[i + 3]] = intcodeR[intcodeR[i + 1]] + intcodeR[intcodeR[i + 2]];
+                        instructionLength = 4;
+                        intcodeR[index[2]] = intcodeR[index[0]] + intcodeR[index[1]];
                         break;
                     case 2:
-                        intcodeR[intcodeR[i + 3]] = intcodeR[intcodeR[i + 1]] * intcodeR[intcodeR[i + 2]];
+                        instructionLength = 4;
+                        intcodeR[index[2]] = intcodeR[index[0]] * intcodeR[index[1]];
                         break;
-                    case 99: _continue = false;
+                    case 3:
+                        instructionLength = 2;
+                        Console.Write("input: ");
+                        intcodeR[index[0]] = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        instructionLength = 2;
+                        Console.Write(intcodeR[index[0]]+" ");
+                        break;
+                    case 5:
+                        if (intcodeR[index[0]] != 0)
+                        {
+                            instructionLength = 0;
+                            i = intcodeR[index[1]];
+                        }
+                        else
+                        {
+                            instructionLength = 3;
+                        }
+                        break;
+                    case 6:
+                        if (intcodeR[index[0]] == 0)
+                        {
+                            instructionLength = 0;
+                            i = intcodeR[index[1]];
+                        }
+                        else
+                        {
+                            instructionLength = 3;
+                        }
+                        break;
+                    case 7:
+                        instructionLength = 4;
+                        if (intcodeR[index[0]]< intcodeR[index[1]])
+                        {
+                            intcodeR[index[2]] = 1;
+                        }
+                        else
+                        {
+                            intcodeR[index[2]] = 0;
+                        }
+                        break;
+                    case 8:
+                        instructionLength = 4;
+                        if (intcodeR[index[0]] == intcodeR[index[1]])
+                        {
+                            intcodeR[index[2]] = 1;
+                        }
+                        else
+                        {
+                            intcodeR[index[2]] = 0;
+                        }
+                        break;
+                    case 99:
+                        _continue = false;
                         break;                       
                 }
             }
             return intcodeR;
+        }
+        private int[] GetIndex(int i,int m1,int m2,int m3,int v1,int v2, int v3)
+        {
+            int i1=0, i2=0, i3=0;
+            switch (m1)
+            {
+                case 0:
+                    i1 = v1;
+                    break;
+                case 1:
+                    i1 = i + 1;
+                    break;
+            }
+            switch (m2)
+            {
+                case 0:
+                    i2 = v2;
+                    break;
+                case 1:
+                    i2 = i + 2;
+                    break;
+            }
+            switch (m3)
+            {
+                case 0:
+                    i3 = v3;
+                    break;
+                case 1:
+                    i3 = i + 3;
+                    break;
+            }
+            return new int[] { i1, i2, i3 };
         }
     }
 }
--- AdventofCode2019/Program.cs
+++ AdventofCode2019/Program.cs
@@ -13,36 +13,41 @@ namespace AdventofCode2019
         {
             //day 1
             Fuel f = new Fuel("../../../mass.txt");
-            Console.WriteLine("day 1 solution: "+f.GetFuel());
+            Console.WriteLine("day 1 solution: " + f.GetFuel());
 
             //day2
-            IntcodeComputer a = new IntcodeComputer("../../../gravity_assist_programm.txt");
-            a.Alarm1202();
-            int[] intcode = a.Run();
+            IntcodeComputer ic1 = new IntcodeComputer("../../../gravity_assist_programm.txt");
+            ic1.Alarm1202();
+            int[] intcode = ic1.Run();
             int nounverb = -1;
-            for(int noun = 0; noun <= 99; noun++)
+            for (int noun = 0; noun <= 99; noun++)
             {
-                for(int verb =0;verb<=99; verb++)
+                for (int verb = 0; verb <= 99; verb++)
                 {
-                    a = new IntcodeComputer("../../../gravity_assist_programm.txt");
-                    a.SetNounVerb(noun, verb);
-                    int[] check = a.Run();
-                    if(check[0]== 19690720)
+                    ic1 = new IntcodeComputer("../../../gravity_assist_programm.txt");
+                    ic1.SetNounVerb(noun, verb);
+                    int[] check = ic1.Run();
+                    if (check[0] == 19690720)
                     {
                         nounverb = 100 * noun + verb;
                     }
                 }
             }
-            Console.WriteLine("day 2 solution: {0} {1}" ,intcode[0],nounverb);
+            Console.WriteLine("day 2 solution: {0} {1}", intcode[0], nounverb);
 
             //day3
             Wires w = new Wires("../../../wires.txt");
-            Console.WriteLine("day 3 solution: {0} {1}",w.GetManhattendistance(),w.GetFewestStepsToIntersection());
+            Console.WriteLine("day 3 solution: {0} {1}", w.GetManhattendistance(), w.GetFewestStepsToIntersection());
 
             //day4
             Password6D p = new Password6D(265275, 781584);
-            Console.WriteLine("day 4 solution: {0} {1}",p.GetPasswordsAmountCrit1(),p.GetPasswordsAmountCrit2());
+            Console.WriteLine("day 4 solution: {0} {1}", p.GetPasswordsAmountCrit1(), p.GetPasswordsAmountCrit2());
 
+            //day5
+
+            Console.WriteLine("day 5 solution: ");
+            IntcodeComputer ic2 = new IntcodeComputer("../../../TEST.txt");
+            ic2.Run();
             Console.ReadKey();
         }
     }
